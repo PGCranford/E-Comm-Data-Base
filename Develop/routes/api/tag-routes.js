6d82b2a8dc90e12787442eb6a9ffd032e3a5940f
PGCranford <PGCranford@gmail.com>
2022-07-29 10:02:55 -0400

    routes updated

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,6 +6,26 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: ProductTag,
+        attributes: [product_tag]
+      }
+    ]
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+
+
+
 });
 
 router.get('/:id', (req, res) => {
